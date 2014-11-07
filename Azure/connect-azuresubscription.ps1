75ae569f642e1a511d2bdd637064f9464f3ccfd7
jasonhimmelstein <jase@sharepointlonghorn.com>
2014-11-06 17:10:56 -0800

    updated
    cleaned up the code

--- Azure/connect-azuresubscription.ps1
+++ Azure/connect-azuresubscription.ps1
@@ -1,23 +1,17 @@
 ﻿#  This script can be used to connect to a specific Azure Subscription if more than one exists in your WindowsAzureProfile.xml file
-#  http://blog.sharepointlonghorn.com/
-#  v1.3 12/11/2013
+#  http://www.sharepointlonghorn.com/
+#  v2.14 11/06/2014
 #  Jason Himmelstein
-
-# Variables block
-$tASub = "VS"
-
 
 # Get the list of Azure Subscriptions you have in your WindowsAzureProfile.xml file 
 Write-Host "The following are the Azure Subscriptions you are connected to:" -ForegroundColor Blue -BackgroundColor Gray
 $xml = (Get-Content -raw -path "C:\Users\$env:username\AppData\Roaming\Windows Azure Powershell\AzureProfile.json") | ConvertFrom-Json
-$xml.Subscriptions | select name
+$xsub = $xml.Subscriptions | Select-Object name | out-gridview -outputmode Single -title "Azure Subscriptions"
 ""
-# Select the Azure Subscription you want to connect to
-$Asub = Read-Host -Prompt "Which Azure Subscription do you wish to connect to? If ""$tAsub"" please press enter otherwise type the name of the Azure Subscriptions you wish to connect to (this is case sensitive)"  
-if ($Asub -eq "") {$Asub = $tAsub}
-
+$ASub = $xsub.Name
+""
 # Connect to the desired Azure Subscription
-Select-AzureSubscription -SubscriptionName $ASub
-
-Write-host "You are now connected to ""$aSub"" and the following Cloud Services are available to you:"
+Write-host "You are now connected to:"
+Select-AzureSubscription -SubscriptionName $ASub | ft name
+Write-host "The following Cloud Services are available to you:"
 Get-AzureService | fl ServiceName,affinitygroup,status
